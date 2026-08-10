be2d7f47a5ac77854b8cca38e6a6d30fc2c95a08
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-10 22:41:23 +0530

    Add solution for MATMINPATH (Java) [CodeSync]

---
+++ CodeChef/Java/MATMINPATH.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int minPathSum(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        int[][] dp = new int[m][n];
+        
+        dp[0][0] = mat[0][0];
+        
+        for(int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + mat[i][0];
+        }
+        
+        for(int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + mat[0][j];
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + mat[i][j];
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
