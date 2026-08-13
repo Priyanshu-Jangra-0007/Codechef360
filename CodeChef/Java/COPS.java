95e7ef869d9be0a101dd8a43c2c46d446c1ef22a
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-13 09:49:42 +0530

    Add solution for COPS (Java) [CodeSync]

--- CodeChef/Java/COPS.java
+++ CodeChef/Java/COPS.java
@@ -1,28 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        
+        while (t-- > 0) {
+            int M = scanner.nextInt();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            int[] copHouses = new int[M];
+            for (int i = 0; i < M; i++) {
+                copHouses[i] = scanner.nextInt();
+            }
+
+            boolean[] safeHouses = new boolean[100]; 
             for (int i = 0; i < 100; i++) {
                 safeHouses[i] = true; 
             }
 
             for (int copHouse : copHouses) {
-                int startHouse = Math.max(1, copHouse - x * 
-                    y);
-                int endHouse = Math.min(100, copHouse + x * 
-                    y);
-                for (int j = startHouse; j <= endHouse; j++) 
-                    {
+                int startHouse = Math.max(1, copHouse - x * y);
+                int endHouse = Math.min(100, copHouse + x * y);
+                for (int j = startHouse; j <= endHouse; j++) {
                     safeHouses[j - 1] = false; 
-                }
-            }
-
-            int countSafeHouses = 0;
-            for (boolean isSafe : safeHouses) {
-                if (isSafe) {
-                    countSafeHouses++;
-                }
-            }
-
-            System.out.println(countSafeHouses);
-        }
-        
-        scanner.close();
-    }
-}
