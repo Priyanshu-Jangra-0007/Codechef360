c83050311e43bf39b055a7f751456c5b7267ffdb
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-20 22:17:09 +0530

    Add solution for RTLL (Java) [CodeSync]

---
+++ CodeChef/Java/RTLL.java
@@ -0,0 +1,15 @@
+
+public static Node listReverse(Node head) {
+    Node prev = null;
+    Node current = head;
+    Node next = null;
+    
+    while (current != null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    
+    return prev;
+}
