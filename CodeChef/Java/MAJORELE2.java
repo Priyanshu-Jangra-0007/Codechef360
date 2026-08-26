deb7e4291896ace3fc24fe690e1aba3f6e83ffb9
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-26 11:54:34 +0530

    Add solution for MAJORELE2 (Java) [CodeSync]

--- CodeChef/Java/MAJORELE2.java
+++ CodeChef/Java/MAJORELE2.java
@@ -1,3 +1,15 @@
+public static List<Long> findSuperstarDishes(List<Long> a, int n) {
+    long el1 = Long.MIN_VALUE, el2 = Long.MIN_VALUE;
+    int cnt1 = 0, cnt2 = 0;
+
+    // Phase 1: Voting
+    for (int i = 0; i < n; i++) {
+        long val = a.get(i);
+
+        if (val == el1) cnt1++;
+        else if (val == el2) cnt2++;
+        else if (cnt1 == 0) {
+            el1 = val;
             cnt1 = 1;
         }
         else if (cnt2 == 0) {
@@ -17,13 +29,3 @@
     for (int i = 0; i < n; i++) {
         long val = a.get(i);
         if (val == el1) cnt1++;
-        else if (val == el2) cnt2++;
-    }
-
-    List<Long> ans = new ArrayList<>();
-    if (cnt1 > n / 3) ans.add(el1);
-    if (cnt2 > n / 3) ans.add(el2);
-
-    Collections.sort(ans);
-    return ans;
-}
