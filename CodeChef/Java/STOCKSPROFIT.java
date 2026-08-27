398f946ed2dc86463554622e913da82c03726b17
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-27 23:56:19 +0530

    Add solution for STOCKSPROFIT (Java) [CodeSync]

--- CodeChef/Java/STOCKSPROFIT.java
+++ CodeChef/Java/STOCKSPROFIT.java
@@ -4,11 +4,9 @@
 
         for (int price : prices) {
             if (price < minPrice) {
-                minPrice = price; // update minimum seen so 
-                    far
+                minPrice = price; // update minimum seen so far
             } else if (price - minPrice > maxProfit) {
-                maxProfit = price - minPrice; // update max 
-                    profit
+                maxProfit = price - minPrice; // update max profit
             }
         }
         return maxProfit;
