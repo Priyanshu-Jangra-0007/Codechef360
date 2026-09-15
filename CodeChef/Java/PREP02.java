1309dd877e266bd91f4001652cd581c0872ca783
Priyanshu . <priyanshujangra145@gmail.com>
2026-09-15 11:20:29 +0530

    Add solution for PREP02 (Java) [CodeSync]

--- CodeChef/Java/PREP02.java
+++ CodeChef/Java/PREP02.java
@@ -1,3 +1,17 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        BufferedReader br=new BufferedReader( new InputStreamReader(System.in)  ); 
+        
         PrintWriter out = new PrintWriter(System.out);
         
         int t = Integer.valueOf(br.readLine());
@@ -15,14 +29,3 @@
                 
                 for (int j = 0 ; j < n ; j++ ) {
                     a[n - i - 1][n - j - 1] = sArr[j];
-                }
-            }
-            
-            for( String[] r : a ){
-                out.println(Arrays.toString(r).replace(",", "").replace("[", "").replace("]", 
-                    "").trim());
-            }
-        }
-        out.close();
-    }
-}
