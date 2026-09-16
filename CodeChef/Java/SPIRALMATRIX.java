6b0f385f735d9bde5646be107076e93e2ead687d
Priyanshu . <priyanshujangra145@gmail.com>
2026-09-16 18:14:43 +0530

    Add solution for SPIRALMATRIX (Java) [CodeSync]

---
+++ CodeChef/Java/SPIRALMATRIX.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix, int m, int n) {
+        List<Integer> result = new ArrayList<>();
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse left to right
+            for (int j = left; j <= right; j++) {
+                result.add(matrix[top][j]);
+            }
+            top++;
+
+            // Traverse top to bottom
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            // Traverse right to left
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    result.add(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+
+            // Traverse bottom to top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
